019c746df2518fe7b20c798226230a22d0c4711b
Joel Smith <joelsmith@users.noreply.github.com>
2023-08-08 11:06:57 -0600

    Use KEDA-specific annotation name for last config (#201)
    Manifestival > v0.7.2 has the ability to use an app-specific annotation
    name so that our changes don't conflict with kubectl-applied changes.
    
    Deps updates:
      go get github.com/manifestival/manifestival@f20c69532c27cff6358cd681be7a477b8c897d54
      go get k8s.io/apiserver@v0.27.3
      go mod tidy
    
    Signed-off-by: Joel Smith <joelsmith@redhat.com>

--- resources/resources_handler.go
+++ resources/resources_handler.go
@@ -9,15 +9,16 @@ import (
 
 const resourcesPath = "keda.yaml"
 const olmResourcesPath = "keda-olm-operator.yaml"
+const LastConfigID = "olm-operator.keda.sh/last-applied-configuration"
 
 func GetResourcesManifest() (mf.Manifest, error) {
 	_, path, _, _ := runtime.Caller(0)
 	fullPath := filepath.Join(filepath.Dir(path), resourcesPath)
 	olmFullPath := filepath.Join(filepath.Dir(path), olmResourcesPath)
-	kedamf, err := mf.NewManifest(fullPath)
+	kedamf, err := mf.NewManifest(fullPath, mf.UseLastAppliedConfigAnnotation(LastConfigID))
 	if err != nil {
 		return kedamf, err
 	}
-	operatormf, err := mf.NewManifest(olmFullPath)
+	operatormf, err := mf.NewManifest(olmFullPath, mf.UseLastAppliedConfigAnnotation(LastConfigID))
 	return kedamf.Append(operatormf), err
 }
